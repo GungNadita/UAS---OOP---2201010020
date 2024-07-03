d3221e8bad62356db3b0113c69d87ebd8d29fb0e
GungNadita <117555233+GungNadita@users.noreply.github.com>
2024-07-03 08:29:59 +0800

    Add files via upload

---
+++ projekUASnadita/src/projekuasnadita/Koneksi.java
@@ -0,0 +1,30 @@
+
+package projekuasnadita;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+
+public class Koneksi {
+    public static Connection con;
+    public static Statement stm;
+    public static Connection configDB()throws SQLException{
+ try {
+ String url
+="jdbc:mysql://localhost/datahotel";
+ String user="root";
+ String pass="";
+ Class.forName("com.mysql.cj.jdbc.Driver");
+ con =(Connection)
+DriverManager.getConnection(url,user,pass);
+ stm = con.createStatement();
+ System.out.println("koneksi berhasil;");
+ } catch (Exception e) {
+ System.err.println("koneksi gagal"
++e.getMessage());
+ }
+return con;
+}
+}
